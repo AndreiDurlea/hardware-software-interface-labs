ae19fc7e0837f204e544cf7c7f20d5bb0cc8f8a7
Andrei Durlea <andrei.durlea.telefon@gmail.com>
2026-03-20 23:33:23 +0200

    lab03-1
    100%

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -4,14 +4,33 @@
 
 #include "binary_search.h"
 
-int binary_search(int *v, int len, int dest)
-{
-	int start = 0;
-	int end = len - 1;
-	int middle;
+int binary_search(int *v, int len, int dest) {
+  int start = 0;
+  int end = len - 1;
+  int middle;
 
-	/**
-	 * TODO: Implement binary search
-	 */
-	return -1;
+check:
+  if (start > end) {
+    goto not_found;
+  }
+  middle = (start + end) / 2;
+
+  if (v[middle] == dest) {
+    goto found;
+  } else if (v[middle] < dest) {
+    goto right;
+  } else {
+    goto left;
+  }
+left:
+  end = middle - 1;
+  goto skip;
+right:
+  start = middle + 1;
+skip:
+  goto check;
+found:
+  return middle;
+not_found:
+  return -1;
 }
