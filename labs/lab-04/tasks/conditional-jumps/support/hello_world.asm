d03622b85fb7939e06d6964ce445a037f8a8210c
Andrei Durlea <andrei.durlea.telefon@gmail.com>
2026-03-26 13:03:41 +0200

    lab04-1

--- labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
+++ labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
@@ -4,6 +4,7 @@
 
 section .data
     myString: db "Hello, World!", 0
+    exitString: db "Goodbye, World!", 0
     N: dq 6                         ; N = 6
 
 section .text
@@ -20,7 +21,7 @@ main:
     mov rax, 2
     mov r8, 1
     cmp rax, r8
-    je print                        ; TODO1: rax > r8?
+    jg print                        ; TODO1: rax > r8?
     xor rax, rax
 
     leave
@@ -28,8 +29,19 @@ main:
 
 print:
     PRINTF64 `%s\n\x0`, myString
-                                    ; TODO2.2: print "Hello, World!" N times
-                                    ; TODO2.1: print "Goodbye, World!"
+    cmp rcx, 0                      ; TODO2.2: print "Hello, World!" N times
+    sub rcx, 1                      ; TODO2.1: print "Goodbye, World!"
+    jne print
+
+    jmp end
+
+    xor rax, rax
+
+    leave
+    ret
+
+end:
+    PRINTF64 `%s\n\x0`, exitString
     xor rax, rax
 
     leave
